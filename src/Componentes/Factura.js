dae8e3bb35d7464425dd414bed612cf9da223482
MoNRaSpGit <ju4nrsuarez@gmail.com>
2025-02-06 02:24:05 -0300

    dsfs

--- src/Componentes/Factura.js
+++ src/Componentes/Factura.js
@@ -1,43 +1,55 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../Css/Factura.css';
+import '../Css/Factura.css';  // Contenedor principal
+import '../Css/logo.css';      // Logo
+import '../Css/Dueno.css';     // Dueño
+import '../Css/Rectangulo.css'; // Rectángulo de información
+
 import logoOrio from '../Img/logoOriol2.png';
 
 const Factura = ({ productosSeleccionados }) => {
     return (
         <div className="factura-container">
-            {/* Cabecera con logo y rectángulo */}
+            {/* Cabecera con logo, caja del dueño y rectángulo de información */}
             <div className="factura-header">
-                {/* Logo de la empresa */}
-               { <div className="factura-logo-container">
-                    <img src={logoOrio} alt="Logo de la empresa" className="factura-logo" />
-                </div>}
 
-                {/* Rectángulo con la información */}
+                {/* Contenedor de logo y dueño agrupados a la izquierda */}
+                <div className="logo-dueno-container">
+                    {/* Contenedor del logo */}
+                    <div className="factura-logo-container">
+                        <img src={logoOrio} alt="Logo de la empresa" className="factura-logo" />
+                    </div>
+
+                    {/* Caja "Dueño" - Rectángulo independiente debajo del logo */}
+                    <div className="dueno">
+                        <div className="dueno-nombre">Oriol Ahirton Acuña</div>
+                        <div className="dueno-nombre">WILSON FERRERIA ALDUNATE 111</div>
+                        <div className="dueno-telefono">092457845</div>
+                    </div>
+                </div>
+
+                {/* Rectángulo con la información alineado a la derecha */}
                 <div className="header-box">
                     <div className="header-row header-rut">RUT EMISOR: 4587854</div>
                     <div className="header-row header-efacture">e-Facture</div>
-                    
-                    {/* Fila 3 con 4 columnas (Títulos en gris) */}
-                    <div className="header-row header-multi header-titles">
-                        <div className="header-col">SERIE</div>
-                        <div className="header-col">NÚMERO</div>
-                        <div className="header-col">PAGO</div>
-                        <div className="header-col">MONEDA</div>
+
+                    {/* Fila combinada de títulos y valores */}
+                    <div className="header-multi">
+                        <div className="header-titles-values">
+                            <div className="header-col header-title">SERIE</div>
+                            <div className="header-col header-title">NÚMERO</div>
+                            <div className="header-col header-title">PAGO</div>
+                            <div className="header-col header-title">MONEDA</div>
+                        </div>
+                        <div className="header-titles-values">
+                            <div className="header-col header-value">A</div>
+                            <div className="header-col header-value">1458754</div>
+                            <div className="header-col header-value">Contado</div>
+                            <div className="header-col header-value">UYU</div>
+                        </div>
                     </div>
 
-                    {/* Fila 4 con 4 columnas (Valores sin fondo gris) */}
-                    <div className="header-row header-multi">
-                        <div className="header-col">A</div>
-                        <div className="header-col">1458754</div>
-                        <div className="header-col">Contado</div>
-                        <div className="header-col">UYU</div>
-                    </div>
-
-                    {/* Fila 5 - RUT RECEPTOR con fondo gris */}
                     <div className="header-row header-receptor">RUT RECEPTOR: 154845845</div>
-
-                    {/* Fila 6 - Información del Cliente con fondo blanco */}
                     <div className="header-row header-cliente">
                         <div>Nombre Cliente</div>
                         <div>Dirección Cliente</div>
@@ -64,7 +76,3 @@ const Factura = ({ productosSeleccionados }) => {
 };
 
 export default Factura;
-
-
-
-/* Version perefecta -->  xt9 */
